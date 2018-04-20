f45560061b9a5aa71c04a8ac2c616e79c972a312
陈可青 <2591282718@qq.com>
2018-04-20 11:28:38 +0800

    指纹解锁/面部解锁

---
+++ 指纹解锁:面部解锁/CCLocalAuthentication/CCLocalAuthentication/CCLocalAuthentication.swift
@@ -0,0 +1,18 @@
+//
+//  CCLocalAuthentication.swift
+//  CCLocalAuthentication
+//
+//  Created by 陈可青 on 2018/4/20.
+//  Copyright © 2018年 colincat. All rights reserved.
+//
+
+import Foundation
+import LocalAuthentication
+
+
+class CCLocalAuthentication: NSObject {
+    
+    
+    
+    
+}
